a7e83593a9e5c5dbed252f699f0cd4d86acebb75
Lakshman Prasad S <lakshman.slp@gmail.com>
2024-09-27 20:33:42 +0530

    2 files are added

--- src/test/java/com/thetestingacademy/Test002.java
+++ src/test/java/com/thetestingacademy/Test002.java
@@ -1,9 +1,8 @@
 package com.thetestingacademy;
 
-import io.restassured.RestAssured;
-
-public class Test001 {
+public class Test002 {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
     }
 }
